d70f0fc5b7633844428b3f5903207196ee171421
Jason Lokerson <cmercenary@gmail.com>
2016-08-18 12:32:53 -0700

    Upgrade to VS2015

--- stdafx.h
+++ stdafx.h
@@ -66,7 +66,7 @@
 #define	__IDxtAlphaSetter_INTERFACE_DEFINED__
 #define	__IDxtJpeg_INTERFACE_DEFINED__
 #define	__IDxtKey_INTERFACE_DEFINED__
-#include <qedit.h>
+#include "qedit.h"
 
 #include <gdiplus.h>
 #include <stdio.h>
